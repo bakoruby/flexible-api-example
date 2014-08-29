02c6c53a8f653d871d115710f9d9ffdd10070023
Frank West <fwest@heiskell.com>
2014-08-28 17:00:21 -0700

    Sanitize includes for models

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::ContactsController < ApplicationController
 
   def index
-    @contacts = Contact.eager_load(symbolize_includes).
+    @contacts = Contact.eager_load(sanitized_includes).
       search(search_params).result
 
     render({json: @contacts}.merge(serializer_includes))
   end
 
   def show
-    @contact = Contact.eager_load(symbolize_includes).
+    @contact = Contact.eager_load(sanitized_includes).
       find(params[:id])
 
     render({json: @contact}.merge(serializer_includes))
@@ -17,9 +17,17 @@ class Api::V1::ContactsController < ApplicationController
   private
 
   def serializer_includes
-    Array(params[:includes]).reduce({}) do |results, include|
+    sanitized_includes.reduce({}) do |results, include|
       results["include_#{include}"] = true
       results
+    end
+  end
+
+
+  def sanitized_includes
+    symbolize_includes.inject([]) do |result, include_param|
+      result << include_param if Contact.reflect_on_association(include_param)
+      result
     end
   end
 
